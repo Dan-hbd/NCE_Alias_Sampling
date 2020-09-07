7349c8e81308650bb087203244f1645fd7c21825
Dan He <dhe@i13hpc58.cluster>
2020-09-07 11:18:24 +0200

    first commit

---
+++ nce/alias_multinomial.py
@@ -0,0 +1,85 @@
+from math import isclose
+import torch
+
+
+class AliasMultinomial(torch.nn.Module):
+    '''Alias sampling method to speedup multinomial sampling
+
+    The alias method treats multinomial sampling as a combination of uniform sampling and
+    bernoulli sampling. It achieves significant acceleration when repeatedly sampling from
+    the save multinomial distribution.
+
+    Attributes:
+        - probs: the probability density of desired multinomial distribution
+
+    Refs:
+        - https://hips.seas.harvard.edu/blog/2013/03/03/the-alias-method-efficient-sampling-with-many-discrete-outcomes/
+    '''
+    def __init__(self, probs):
+        super(AliasMultinomial, self).__init__()
+
+        assert isclose(probs.sum().item(), 1), 'The noise distribution must sum to 1'
+        cpu_probs = probs.cpu()
+        K = len(probs)
+
+        # such a name helps to avoid the namespace check for nn.Module
+        self_prob = [0] * K
+        self_alias = [0] * K
+
+        # Sort the data into the outcomes with probabilities
+        # that are larger and smaller than 1/K.
+        smaller = []
+        larger = []
+        for idx, prob in enumerate(cpu_probs):
+            self_prob[idx] = K*prob
+            if self_prob[idx] < 1.0:
+                smaller.append(idx)
+            else:
+                larger.append(idx)
+
+        # Loop though and create little binary mixtures that
+        # appropriately allocate the larger outcomes over the
+        # overall uniform mixture.
+        # 通过拼凑，将各个类别都凑为1
+
+        while len(smaller) > 0 and len(larger) > 0:
+            small = smaller.pop()
+            large = larger.pop()
+
+            self_alias[small] = large
+            self_prob[large] = (self_prob[large] - 1.0) + self_prob[small]
+
+            if self_prob[large] < 1.0:
+                smaller.append(large)
+            else:
+                larger.append(large)
+        # self_prob 得到原类型的概率
+        # Alias 第二种类型
+
+        for last_one in smaller+larger:
+            self_prob[last_one] = 1
+
+        self.register_buffer('prob', torch.Tensor(self_prob))
+
+        # self_prob 中第四个和第五个元素概率为1，self_alias表格中，对应的是0，而其余的均不是0
+        # 参考 https://shomy.top/2017/05/09/alias-method-sampling/
+        self.register_buffer('alias', torch.LongTensor(self_alias))
+
+    def draw(self, *size):
+        """Draw N samples from multinomial
+
+        Args:
+            - size: the output size of samples
+        """
+        max_value = self.alias.size(0)   # max_value应该是 V 吧
+        # size: 负样本的个数，每一个元算的value是0到V-1
+        kk = self.alias.new(*size).random_(0, max_value).long().view(-1)
+        prob = self.prob[kk]  # 抽取kk[i]到这个token的概率
+        alias = self.alias[kk]  # 如果不取值 kk[i]， 则取alias[i] 这个token
+        # b is whether a random number is smaller than prob, 小于输出1
+        b = torch.bernoulli(prob).long()  # 如果随机数小于prob， 则取kk[i]
+        oq = kk.mul(b)
+        oj = alias.mul(1 - b)  # 随机数大于prob， b为0， 1-b为1，取alias[i]
+
+        return (oq + oj).view(size)
+
---
+++ sample.py
@@ -0,0 +1,29 @@
+"""A minimal sample script for illustration of basic usage of NCE module"""
+
+import torch
+from nce import IndexLinear
+
+class_freq = [1, 2, 3, 4, 5, 6, 7]  # an unigram class probability
+freq_count = torch.FloatTensor(class_freq)
+print("total counts for all tokens:", freq_count.sum())
+noise = freq_count / freq_count.sum()
+
+# IndexLinear 继承了NCELoss 类
+nce_linear = IndexLinear(
+    embedding_dim=100,  # input dim
+    num_classes=300000,  # output dim
+    noise=noise,
+)
+
+# 这里 input 假装是经过了 embedding之后的
+input = torch.Tensor(200, 100)  # [batch, emb_dim]
+# target中这里是ones， 但是我们的task中应该是 对应的正确的token的id
+target = torch.ones(200, 1).long()  # [batch, emb_dim]
+# training mode
+loss = nce_linear(target, input).mean()
+print(loss.item())
+
+# evaluation mode for fast probability computation
+nce_linear.eval()
+prob = nce_linear(target, input).mean()
+print(prob.item())
